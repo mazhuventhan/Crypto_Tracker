699cb42981a032db51868202c87c23e3790493ad
NizanthLinkWorks <venthanmazhu@gmail.com>
2025-09-22 10:19:39 +0530

    Final changes

--- src/store/sagas/portfolioSaga.ts
+++ src/store/sagas/portfolioSaga.ts
@@ -1,10 +1,10 @@
 import { takeEvery, select } from "redux-saga/effects";
-import { addToPortfolio, updateQuantity, removeFromPortfolio } from "../slices/portfolioSlice";
+import { addToPortfolio, updateQuantity, removeFromPortfolio, clearPortfolio } from "../slices/portfolioSlice";
 import type { RootState } from "../index";
 function* savePortfolioToLocalStorage() {
   const portfolio: RootState['portfolio']['items'] = yield select(state => state.portfolio.items);
   localStorage.setItem("portfolio", JSON.stringify(portfolio));
 }
 export default function* portfolioSaga() {
-  yield takeEvery([addToPortfolio.type, updateQuantity.type, removeFromPortfolio.type], savePortfolioToLocalStorage);
+  yield takeEvery([addToPortfolio.type, updateQuantity.type, removeFromPortfolio.type, clearPortfolio.type], savePortfolioToLocalStorage);
 }
